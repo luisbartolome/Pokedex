fb470fedcb509baaa1d4ecee437c76177c42d811
luisbartolome <luisbartolomeoyarzabal@hotmail.es>
2021-04-26 19:59:25 +0200

    ci: pokedex proyect

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-alert('Hello World');
-
-let favoriteFood = 'spanish food';
-document.write(favoriteFood);
+let pokemonRepository = (function() {
+            let pokemonList = [{
+                    name: "Bulbasaur",
+                    types: ['grass', 'poison'],
+                    height: 7
+                },
+                {
+                    name: "Charmander",
+                    types: ['fire'],
+                    height: 6
+                },
+                {
+                    name: "Beedrill",
+                    types: ['bug', 'poison'],
+                    height: 1
+                }
+            ];
