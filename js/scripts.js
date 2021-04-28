2eae582a2e4f361a6f916c81ed93211676431abe
luisbartolome <luisbartolomeoyarzabal@hotmail.es>
2021-04-28 21:20:02 +0200

    refactor change code

--- js/scripts.js
+++ js/scripts.js
@@ -14,32 +14,3 @@ let pokemonList = [{
         height: 4
     }
 ];
-
-var pokemon = ["Wartortle", "Charmander", "Nidoran"]
-
-for (i = 0; pokemon.lengt; i++) {
-    document.write(pokemon);
-}
-
-let repository = [{
-        name: "Wartortle",
-        type: 'water',
-        height: 1
-    },
-    {
-        name: "Charmander",
-        type: 'fire',
-        height: 6
-    },
-    {
-        name: "Nidoran",
-        type: 'poison',
-        height: 4
-    }
-];
-
-var height = 6;
-
-if (height >= 6) {
-    console.log("Wow, that`s big.");
-}
