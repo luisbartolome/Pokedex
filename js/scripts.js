17fbc4cdf2025b8d57afd9c756ed04a9a1ca7a96
luisbartolome <luisbartolomeoyarzabal@hotmail.es>
2021-04-29 23:12:08 +0200

    compilation added document.write lines code

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 let pokemonList = [{
         name: "Wartortle",
-        types: 'water',
+        types: ['water'],
         height: 1
     },
     {
         name: "Charmander",
-        types: 'fire',
+        types: ['fire'],
         height: 6
     },
     {
         name: "Nidoran",
-        types: 'poison',
+        types: ['poison'],
         height: 4
     }
 ];
@@ -27,9 +27,16 @@ for (let i = 0; i < pokemonList.length; i++) {
 // The conditional check if the height is above a certain value
 //using <b> tag to bold part of a tring display on page
 
+let i = 1;
+
 if (pokemonList[i].height > 5) {
 
     document.write('<b>- Wow, that\'s big!.</b>');
 
     document.write('<ul>');
+    document.write('<li><br>');
+    document.write('</li><br>');
+    document.write('</ul>');
+
+
 };
