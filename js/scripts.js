2c188e837c20af42297efec1e43d757e5c56b302
luisbartolome <luisbartolomeoyarzabal@hotmail.es>
2021-04-29 19:39:53 +0200

    compilation: document.write ('<ul>') and style types

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 let pokemonList = [{
         name: "Wartortle",
-        type: 'water',
+        types: 'water',
         height: 1
     },
     {
         name: "Charmander",
-        type: 'fire',
+        types: 'fire',
         height: 6
     },
     {
         name: "Nidoran",
-        type: 'poison',
+        types: 'poison',
         height: 4
     }
 ];
@@ -19,11 +19,10 @@ let pokemonList = [{
 
 document.write('<ul class="pokemon-list">');
 
-for (let i = 0; pokemonList.length; i++)
-
-// Used + string concatenation instead of ${} (string interpolation)
+for (let i = 0; i < pokemonList.length; i++) {
 
     document.write('<li class="pokemon-list__item"><b>' + pokemonList[i].name + '</b> (height: ' + pokemonList[i].height + ')');
+};
 
 // The conditional check if the height is above a certain value
 //using <b> tag to bold part of a tring display on page
@@ -31,4 +30,6 @@ for (let i = 0; pokemonList.length; i++)
 if (pokemonList[i].height > 5) {
 
     document.write('<b>- Wow, that\'s big!.</b>');
-}
+
+    document.write('<ul>');
+};
