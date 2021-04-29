035c70624be87ebdb66cc9e687d5873fd8a34f53
luisbartolome <luisbartolomeoyarzabal@hotmail.es>
2021-04-29 11:25:15 +0200

    ci: "for" loop changes and conditional "if".

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,21 @@ let pokemonList = [{
         height: 4
     }
 ];
+
+//Display the data on the page as a unordered list
+
+document.write('<ul class="pokemon-list">');
+
+for (let i = 0; pokemonList.length; i++)
+
+// Used + string concatenation instead of ${} (string interpolation)
+
+    document.write('<li class="pokemon-list__item"><b>' + pokemonList[i].name + '</b> (height: ' + pokemonList[i].height + ')');
+
+// The conditional check if the height is above a certain value
+//using <b> tag to bold part of a tring display on page
+
+if (pokemonList[i].height > 5) {
+
+    document.write('<b>- Wow, that\'s big!.</b>');
+}
