9bfeec5ee0c5ffe110e439a5cf945a343d78a607
luisbartolome <luisbartolomeoyarzabal@hotmail.es>
2021-04-30 13:45:39 +0200

    style organize the code and delete document.write lines

--- js/scripts.js
+++ js/scripts.js
@@ -20,16 +20,7 @@ let pokemonList = [{
 document.write('<ul class="pokemon-list">');
 
 for (let i = 0; i < pokemonList.length; i++) {
-
     document.write('<li class="pokemon-list__item"><b>' + pokemonList[i].name + '</b> (height: ' + pokemonList[i].height + ')');
-};
-
-// The conditional check if the height is above a certain value
-//using <b> tag to bold part of a tring display on page
-
-// List all pokemons, emphasis on height
-
-for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height > 1) {
         document.write(
             pokemonList[i].name + "'s height is " + pokemonList[i].height + "m" + " - big pokemon" + "<br>")
@@ -39,7 +30,4 @@ for (let i = 0; i < pokemonList.length; i++) {
     }
 };
 
-document.write('<ul>');
-document.write('<li><br>');
-document.write('</li><br>');
 document.write('</ul>');
