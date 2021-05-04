4a307cc369fc0b16627ea762485f548cae1c2094
luisbartolome <luisbartolomeoyarzabal@hotmail.es>
2021-05-04 14:15:00 +0200

    refactor javascript code.clean code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,61 @@
-let pokemonList = [{
-        name: "Wartortle",
-        types: ['water'],
-        height: 3
-    },
-    {
-        name: "Charmander",
-        types: ['fire'],
-        height: 0.9
-    },
-    {
-        name: "Nidoran",
-        types: ['poison'],
-        height: 1
-    }
-];
+let pokemonRepository = (function() {
+    let pokemonList = [{
+            name: "Wartortle",
+            types: ['water'],
+            height: 3
+        },
+        {
+            name: "Charmander",
+            types: ['fire'],
+            height: 0.9
+        },
+        {
+            name: "Nidoran",
+            types: ['poison'],
+            height: 1
+        }
+    ];
 
-//Display the data on the page as a unordered list
+
+    // add the Pokémon referred to with item to the pokemonList array +  datatype check
+
+    function add(pokemon) {
+        if (typeof pokemon === 'object' && typeof pokemon !== null) {
+            pokemonList.push(pokemon);
+        } else {
+            alert('type of parameter is not an object');
+        }
+    }
+
+    //return the pokemonList array
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    };
+
+})();
+
+//add the correct type of data to pokemonList array
+pokemonRepository.add({
+    name: 'Voltorb',
+    height: 0.5,
+    types: ['electric']
+});
 
 document.write('<ul class="pokemon-list">');
 
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write('<li class="pokemon-list__item"><b>' + pokemonList[i].name + '</b> (height: ' + pokemonList[i].height + ')');
-    if (pokemonList[i].height > 1) {
-        document.write(" - big pokemon");
-    } else {
-        document.write(" - small pokemon");
-    }
-    document.write("</li>")
+//move the function declaration passed to forEach() to make things clearer
+function myLoopFunction(pokemon) {
+    document.write('<li class="pokemon-list__item"><b>' + pokemon.name + '</b> (height: ' + pokemon.height + ')' + "<br>" + pokemon.types);
+
+    document.write('</li><br>');
 };
-document.write('</ul>');
+
+//forEach Loop iterates each pokemon name and height.
+pokemonRepository.getAll().forEach(myLoopFunction);
+
+document.write('<ul>');
