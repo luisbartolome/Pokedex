9e50a00da7742f896fdcd9eb66e632fd3920299e
luisbartolome <luisbartolomeoyarzabal@hotmail.es>
2021-05-07 11:59:59 +0200

    style adding height to button.innerText.

--- js/scripts.js
+++ js/scripts.js
@@ -42,7 +42,7 @@ let pokemonRepository = (function() {
 
         //pokemon names on the buttons
         let button = document.createElement('button');
-        button.innerText = pokemon.name + '</br> (height: ' + pokemon.height + ')' + "<br>" + pokemon.types;
+        button.innerText = pokemon.name + ' (height: ' + pokemon.height + ')' + "<br>" + pokemon.types;
         button.classList.add('button-class');
         //Event listener on click
         button.addEventListener('click', function(event) {
