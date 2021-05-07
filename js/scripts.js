9a0b0b6c33dcde8bb547f7480df6f77f157d269b
luisbartolome <luisbartolomeoyarzabal@hotmail.es>
2021-05-07 11:49:52 +0200

    style:adding info in button.innerText

--- js/scripts.js
+++ js/scripts.js
@@ -42,7 +42,7 @@ let pokemonRepository = (function() {
 
         //pokemon names on the buttons
         let button = document.createElement('button');
-        button.innerText = pokemon.name + ' : ' + pokemon.height + ' Cm ';
+        button.innerText = pokemon.name + '</br> (height: ' + pokemon.height + ')' + "<br>" + pokemon.types;
         button.classList.add('button-class');
         //Event listener on click
         button.addEventListener('click', function(event) {
