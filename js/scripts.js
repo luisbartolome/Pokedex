fcab2cc691d0c13d2f16fd31da4b187fe6da3c1b
luisbartolome <luisbartolomeoyarzabal@hotmail.es>
2021-05-10 18:41:00 +0200

    compilation: fixing the showDetails function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,21 @@
 let pokemonRepository = (function() {
     let pokemonList = [{
             name: "Wartortle",
-            types: ['water'],
-            height: 3
+            types: ["water"],
+            height: 3,
         },
         {
             name: "Charmander",
-            types: ['fire'],
-            height: 0.9
+            types: ["fire"],
+            height: 0.9,
         },
         {
             name: "Nidoran",
-            types: ['poison'],
-            height: 1
-        }
+            types: ["poison"],
+            height: 1,
+        },
     ];
-
     //move the function declaration passed to forEach() to make things clearer
-
     function add(pokemon) {
         if (
             typeof pokemon === "object" &&
@@ -30,56 +28,47 @@ let pokemonRepository = (function() {
             console.log("pokemon is not correct");
         }
     }
-
-    //return the repository array  
+    //return the repository array
     function getAll() {
         return pokemonList;
     }
-
     //creating lists and button in the DOM
-
     function addListItem(pokemon) {
-
-        let pokemonList = document.querySelector('.pokemon-list');
-        let listpokemon = document.createElement('card');
-
+        let pokemonList = document.querySelector(".pokemon-list");
+        let listpokemon = document.createElement("card");
         //pokemon names on the buttons
-        let button = document.createElement('button');
-        button.innerText = pokemon.name + ' (height: ' + pokemon.height + ') ' + pokemon.types;
-        button.classList.add('button-class');
+        let button = document.createElement("button");
+        button.innerText =
+            pokemon.name + " (height: " + pokemon.height + ") " + pokemon.types;
+        button.classList.add("button-class");
         listpokemon.appendChild(button);
         pokemonList.appendChild(listpokemon);
         //Event listener on click
-        button.addEventListener('click', function(event) {
-            showDetails(pokemon)
-        })
-    };
-
-    //ShowDetails function
-
-    function showDetails(pokemon) {
-        console.log(pokemon);
+        button.addEventListener("click", function(event) {
+            showDetails(pokemon);
+        });
     }
-
-
+    //ShowDetails function
+    function showDetails(pokemon) {
+        if (pokemon.height > 1) {
+            console.log("your Pokemon is " + pokemon.name + "and is a big Pokemon");
+        } else {
+            console.log(pokemon.name + " is a small Pokemon");
+        }
+    }
     return {
         getAll: getAll,
         add: add,
         addListItem: addListItem,
     };
-
-});
-
+})();
 //add the correct type of data to pokemonList array
-
 pokemonRepository.add({
-    name: 'Voltorb',
+    name: "Voltorb",
     height: 0.5,
-    types: ['electric']
+    types: ["electric"],
 });
-
 console.log(pokemonRepository.getAll());
-
 //forEach Loop iterates each pokemon name in a button in an unorderd list
 pokemonRepository.getAll().forEach(function(pokemon) {
     pokemonRepository.addListItem(pokemon);
